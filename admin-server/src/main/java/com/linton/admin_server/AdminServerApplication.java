5a51dc795c4cfa083f4d18114f8bebd49bc8cdce
Lintonch <lintonchoudhury1993@gmail.com>
2025-05-21 16:17:40 +0530

    inventory and order and gateway changes

--- admin-server/src/main/java/com/linton/admin_server/AdminServerApplication.java
+++ admin-server/src/main/java/com/linton/admin_server/AdminServerApplication.java
@@ -3,6 +3,8 @@ package com.linton.admin_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+@EnableAdminServer
 @SpringBootApplication
 public class AdminServerApplication {
 
